567be558a7c45fa6dd2f40a4ecd064bd1af09d03
Ahmadosh-Hamad <ahmad.hamad@harri.com>
2026-03-02 14:40:21 +0200

    enable import

--- import-variables.tf
+++ import-variables.tf
@@ -1,11 +1,5 @@
 variable "enable_import" {
   description = "Enable import of ECR repositories"
   type        = bool
-  default     = true
-}
-
-variable "import_ecr_repositories" {
-  description = "Map of ECR repository keys to import IDs"
-  type        = map(string)
-  default     = {}
+  default     = false
 }
